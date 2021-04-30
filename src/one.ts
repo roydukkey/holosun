9319e4c66eb503d6b85189d428e3bc863f638311
roydukkey <contact@changelog.me>
2021-04-30 15:17:44 -0400

    Add generics for `.one()`

--- src/one.ts
+++ src/one.ts
@@ -2,23 +2,29 @@
 // Copyright (c) roydukkey. All rights reserved.                     //
 // ================================================================= //
 
+import type { EventMap } from './EventMap';
 import { isOnceSupported } from './support/once';
 import { oneAugmenterKey, single } from './support/single';
 
 
 /**
- * Attach an event listener that will be called once whenever each of the specified event types are delivered to the given target.
+ * Attach an event listener that will be called once whenever the specified event type is delivered to the given target.
  *
  * @param target - The target on which the event listener is attached.
- * @param types - A case-sensitive string representing the {@link https://developer.mozilla.org/en-US/docs/Web/Events|event type}s to listen for.
+ * @param type - A case-sensitive string representing the {@link https://developer.mozilla.org/en-US/docs/Web/Events|event type} to listen for.
  * @param listener - The object that receives a notification (an object that implements the {@link https://developer.mozilla.org/en-US/docs/Web/API/Event|Event} interface) when an event of a specified type occurs. This must be an object implementing the {@link https://developer.mozilla.org/en-US/docs/Web/API/EventListener|EventListener} interface, or a function.
- * @param useCapture - Whether or not events of these types will be dispatched to the registered listener before being dispatched to any target beneath it in the DOM tree.
+ * @param useCaptureOrOptions - A Boolean indicating whether events of this type will be dispatched to the registered listener before being dispatched to any target beneath it in the DOM tree; or an {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#parameters|options object} specifying characteristics about the event listener.
  *
  * @returns The provided listener when successfully attached; otherwise, `undefined`.
  *
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#the_event_listener_callback} The event listener callback for details on the callback itself.
  */
-export function one (target: EventTarget, types: string, listener: EventListenerOrEventListenerObject | null, useCapture?: boolean): EventListenerOrEventListenerObject | null | undefined;
+ export function one <T extends EventTarget, E extends keyof EventMap<T> & string, R> (
+	target: T,
+	type: E,
+	listener: (this: T, event: EventMap<T>[E]) => R,
+	useCaptureOrOptions?: boolean | AddEventListenerOptions
+): typeof listener | undefined;
 
 
 /**
@@ -27,13 +33,60 @@ export function one (target: EventTarget, types: string, listener: EventListener
  * @param target - The target on which the event listener is attached.
  * @param types - A case-sensitive string representing the {@link https://developer.mozilla.org/en-US/docs/Web/Events|event type}s to listen for.
  * @param listener - The object that receives a notification (an object that implements the {@link https://developer.mozilla.org/en-US/docs/Web/API/Event|Event} interface) when an event of a specified type occurs. This must be an object implementing the {@link https://developer.mozilla.org/en-US/docs/Web/API/EventListener|EventListener} interface, or a function.
- * @param options - An {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#parameters|options object} specifying characteristics about the event listener.
+ * @param useCaptureOrOptions - A Boolean indicating whether events of these types will be dispatched to the registered listener before being dispatched to any target beneath it in the DOM tree; or an {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#parameters|options object} specifying characteristics about the event listener.
  *
  * @returns The provided listener when successfully attached; otherwise, `undefined`.
  *
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#the_event_listener_callback} The event listener callback for details on the callback itself.
  */
-export function one (target: EventTarget, types: string, listener: EventListenerOrEventListenerObject | null, options: AddEventListenerOptions): EventListenerOrEventListenerObject | null | undefined;
+export function one <T extends EventTarget, E extends Event, R> (
+	target: T,
+	types: string,
+	listener: (this: T, event: E) => R,
+	useCaptureOrOptions?: boolean | AddEventListenerOptions
+): typeof listener | undefined;
+
+
+/**
+ * Attach an event listener that will be called once whenever each of the specified event types are delivered to the given target.
+ *
+ * @param target - The target on which the event listener is attached.
+ * @param types - A case-sensitive string representing the {@link https://developer.mozilla.org/en-US/docs/Web/Events|event type}s to listen for.
+ * @param listener - The object that receives a notification (an object that implements the {@link https://developer.mozilla.org/en-US/docs/Web/API/Event|Event} interface) when an event of a specified type occurs. This must be an object implementing the {@link https://developer.mozilla.org/en-US/docs/Web/API/EventListener|EventListener} interface, or a function.
+ * @param useCaptureOrOptions - A Boolean indicating whether events of these types will be dispatched to the registered listener before being dispatched to any target beneath it in the DOM tree; or an {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#parameters|options object} specifying characteristics about the event listener.
+ *
+ * @returns The provided listener when successfully attached; otherwise, `undefined`.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#the_event_listener_callback} The event listener callback for details on the callback itself.
+ */
+export function one <T extends EventTarget, L extends EventListenerObject | null> (
+	target: T,
+	types: string,
+	listener: L,
+	useCaptureOrOptions?: boolean | AddEventListenerOptions
+): L | undefined;
+
+
+/**
+ * Attach an event listener that will be called once whenever the specified event type is delivered to the given node from specific descendants.
+ *
+ * @param node - The node on which the event listener is attached.
+ * @param selector - A selector string to filter the descendants of the given node that trigger the event.
+ * @param type - A case-sensitive string representing the {@link https://developer.mozilla.org/en-US/docs/Web/Events|event type} to listen for.
+ * @param listener - The object that receives a notification (an object that implements the {@link https://developer.mozilla.org/en-US/docs/Web/API/Event|Event} interface) when an event of a specified type occurs. This must be an object implementing the {@link https://developer.mozilla.org/en-US/docs/Web/API/EventListener|EventListener} interface, or a function.
+ * @param useCaptureOrOptions - A Boolean indicating whether events of this type will be dispatched to the registered listener before being dispatched to any target beneath it in the DOM tree; or an {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#parameters|options object} specifying characteristics about the event listener.
+ *
+ * @returns The provided listener when successfully attached; otherwise, `undefined`.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#the_event_listener_callback} The event listener callback for details on the callback itself.
+ */
+export function one <T extends Node, E extends keyof EventMap<T>, R> (
+	node: T,
+	selector: string,
+	type: E,
+	listener: (this: T, event: EventMap<T>[E]) => R,
+	useCaptureOrOptions?: boolean | AddEventListenerOptions
+): typeof listener | undefined;
 
 
 /**
@@ -43,13 +96,19 @@ export function one (target: EventTarget, types: string, listener: EventListener
  * @param selector - A selector string to filter the descendants of the given node that trigger the event.
  * @param types - A case-sensitive string representing the {@link https://developer.mozilla.org/en-US/docs/Web/Events|event type}s to listen for.
  * @param listener - The object that receives a notification (an object that implements the {@link https://developer.mozilla.org/en-US/docs/Web/API/Event|Event} interface) when an event of a specified type occurs. This must be an object implementing the {@link https://developer.mozilla.org/en-US/docs/Web/API/EventListener|EventListener} interface, or a function.
- * @param useCapture - Whether or not events of these types will be dispatched to the registered listener before being dispatched to any target beneath it in the DOM tree.
+ * @param useCaptureOrOptions - A Boolean indicating whether events of these types will be dispatched to the registered listener before being dispatched to any target beneath it in the DOM tree; or an {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#parameters|options object} specifying characteristics about the event listener.
  *
  * @returns The provided listener when successfully attached; otherwise, `undefined`.
  *
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#the_event_listener_callback} The event listener callback for details on the callback itself.
  */
-export function one (node: Node, selector: string, types: string, listener: EventListenerOrEventListenerObject | null, useCapture?: boolean): EventListenerOrEventListenerObject | null | undefined;
+export function one <T extends Node, E extends Event, R> (
+	node: T,
+	selector: string,
+	types: string,
+	listener: (this: T, event: E) => R,
+	useCaptureOrOptions?: boolean | AddEventListenerOptions
+): typeof listener | undefined;
 
 
 /**
@@ -59,28 +118,52 @@ export function one (node: Node, selector: string, types: string, listener: Even
  * @param selector - A selector string to filter the descendants of the given node that trigger the event.
  * @param types - A case-sensitive string representing the {@link https://developer.mozilla.org/en-US/docs/Web/Events|event type}s to listen for.
  * @param listener - The object that receives a notification (an object that implements the {@link https://developer.mozilla.org/en-US/docs/Web/API/Event|Event} interface) when an event of a specified type occurs. This must be an object implementing the {@link https://developer.mozilla.org/en-US/docs/Web/API/EventListener|EventListener} interface, or a function.
- * @param options - An {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#parameters|options object} specifying characteristics about the event listener.
+ * @param useCaptureOrOptions - A Boolean indicating whether events of these types will be dispatched to the registered listener before being dispatched to any target beneath it in the DOM tree; or an {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#parameters|options object} specifying characteristics about the event listener.
  *
  * @returns The provided listener when successfully attached; otherwise, `undefined`.
  *
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#the_event_listener_callback} The event listener callback for details on the callback itself.
  */
-export function one (node: Node, selector: string, types: string, listener: EventListenerOrEventListenerObject | null, options: AddEventListenerOptions): EventListenerOrEventListenerObject | null | undefined;
+export function one <T extends Node, L extends EventListenerObject | null> (
+	node: T,
+	selector: string,
+	types: string,
+	listener: L,
+	useCaptureOrOptions?: boolean | AddEventListenerOptions
+): L | undefined;
 
 
 /**
- * Attach an event listener that will be called once whenever each of the specified event types are delivered to the given target or node.
+ * Attach an event listener that will be called once whenever the specified event type is delivered to the given target or node.
  *
  * @remarks
- * This ambiguous signature enables other libraries to forward configuration up the chain without branching logic.
+ * These ambiguous signatures enable other libraries to forward configuration up the chain without branching logic.
  */
-export function one (
-	targetOrNode: EventTarget,
+export function one <T extends EventTarget, E extends keyof EventMap<T>, R> (
+	targetOrNode: T,
+	selector: string | null,
+	type: E,
+	listener: (this: T, event: EventMap<T>[E]) => R,
+	useCaptureOrOptions?: boolean | AddEventListenerOptions
+): typeof listener | undefined;
+
+
+export function one <T extends EventTarget, E extends Event, R> (
+	targetOrNode: T,
 	selector: string | null,
 	types: string,
-	listener: EventListenerOrEventListenerObject | null,
+	listener: (this: T, event: E) => R,
 	useCaptureOrOptions?: boolean | AddEventListenerOptions
-): EventListenerOrEventListenerObject | null | undefined;
+): typeof listener | undefined;
+
+
+export function one <T extends EventTarget, L extends EventListenerObject | null> (
+	targetOrNode: T,
+	selector: string | null,
+	types: string,
+	listener: L,
+	useCaptureOrOptions?: boolean | AddEventListenerOptions
+): L | undefined;
 
 
 /**
